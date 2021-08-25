44d26c3e8c61e76caa930816f42c6ec7029003d2
Jordan Hammonds <Jordanhammonds1999@ou.edu>
2021-08-25 15:35:22 -0500

    github

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+// Jordan Hammonds 
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I feel I'm fairly comfortable with the coding we did in 3013 and understand the basic. Github is easy to use as well and should not be a huge problem");
+        }
+    }
+}
